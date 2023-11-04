ec62b6cf9cafe88060f5e40ffdcc800ca046de7c
sittipak.sri <sittipak.ssw@gmail.com>
2023-11-04 22:57:15 +0700

    Initial Commit

---
+++ Slide_Number_Puzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slide_Number_Puzzle
+//
+//  Created by Sittipak Srisawas on 4/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slide_Number_Puzzle/Slide_Number_PuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slide_Number_PuzzleApp.swift
+//  Slide_Number_Puzzle
+//
+//  Created by Sittipak Srisawas on 4/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Slide_Number_PuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
